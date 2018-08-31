b0a8924ae337d907691ae9da4f66984cd3dba20f
Ashish Pathak <ashish.k.pathak@gmail.com>
2018-08-31 10:23:12 +1000

    Added Echo Controller

--- src/main/java/au/com/pathak/springbootdemo/controller/EchoController.java
+++ src/main/java/au/com/pathak/springbootdemo/controller/EchoController.java
@@ -1,4 +1,30 @@
 package au.com.pathak.springbootdemo.controller;
 
+import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EchoController {
+
+
+
+  @RequestMapping("/echo")
+  public Map<String,String> echoRequestParams(HttpServletRequest request) {
+
+    Map<String, String> response = new LinkedHashMap<>();
+    Enumeration<String> headerNames = request.getHeaderNames();
+
+    while(headerNames.hasMoreElements()) {
+      String key = headerNames.nextElement();
+      response.put(key,request.getHeader(key));
+
+    }
+
+    return response;
+  }
 }
